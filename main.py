f069a111cd79eb7fc5874ef3b55e700b90b31b9c
0wlie22 <dari.sedova@gmail.com>
2023-08-01 21:56:26 +0300

    Add block argument

--- main.py
+++ main.py
@@ -24,19 +24,25 @@ def get_answer(question_number: int) -> str:
         return answers[question_number - 1]
 
 
-def ask_question(question, answers, correct_answer):
-    print_colored(question, Fore.YELLOW)
+def ask_question(question, answers, correct_answer) -> bool:
+    print_colored("\n" + question.strip(), Fore.CYAN)
     for _, answer in enumerate(answers):
-        print_colored(answer)
-    user_answer = input("Enter your answer: ")
+        print_colored(answer.strip())
+    user_answer = input(f"{Fore.YELLOW}Enter your answer: {Style.RESET_ALL}")
     if user_answer == correct_answer:
         print_colored("Correct!", Fore.GREEN)
+        return True
     else:
         print_colored(f"Wrong! Correct answer is {correct_answer}", Fore.RED)
+        return False
     
 
-def main(start_question, end_qustion):
+def main(start_question, end_qustion, block=-1):
+    if block >= 0:
+        start_question = block * 10
+        end_qustion = block * 10 + 9
     print_flag = False
+    question_count = 0
     with open("input.txt", "r") as f:
         for line in f:
             line_type = parse_line(line)
@@ -46,21 +52,25 @@ def main(start_question, end_qustion):
             if line_type == "question" and int(line.split(".")[0]) == start_question:
                 print_flag = True
             if line_type == "question" and int(line.split(".")[0]) == end_qustion + 1:
+                questions_total = end_qustion - start_question + 1
+                print_colored(f"\nYou answered {question_count}/{questions_total}", Fore.CYAN)
                 print_flag = False
                 return
 
             if print_flag:
-                ask_question(line, [next(f) for _ in range(5)],
-                             get_answer(int(line.split(".")[0])))
+                result = ask_question(line, [next(f) for _ in range(5)], get_answer(int(line.split(".")[0])))
+                if result:
+                    question_count += 1
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", type=int, default=1, help="start question number")
     parser.add_argument("-e", type=int, default=285, help="end question number")
+    parser.add_argument("-b", type=int, help="block number")
     args = parser.parse_args()
     try:
-        main(args.s, args.e)
+        main(args.s, args.e, args.b)
     except KeyboardInterrupt:
-        print("\nExiting...")
+        print_colored("\nExiting...", Fore.RED)
 
