3a183c6a0d78b2efc103c95b4ecb6cf5ff5a4914
0wlie22 <dari.sedova@gmail.com>
2023-08-05 23:33:08 +0300

    Refactor code

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import re
 import argparse
 import os
+import re
 
 from colorama import Fore, Style
 
-def print_colored(text, color=Fore.WHITE):
+INPUT_FILE = "input.txt"
+WRONG_QUESTIONS_FILE = "wrong_questions.txt"
+
+
+def print_colored(text, color=Fore.WHITE) -> None:
     print(f"{color}{text}{Style.RESET_ALL}")
 
 
@@ -31,23 +35,27 @@ def ask_question(question, answers, correct_answer) -> bool:
     for _, answer in enumerate(answers):
         print_colored(answer.strip())
     user_answer = input(f"{Fore.YELLOW}Enter your answer: {Style.RESET_ALL}")
+    while user_answer not in ["a", "b", "c", "d"]:
+        user_answer = input(f"{Fore.RED}Wrong answer!\n{Fore.YELLOW}Enter your answer: {Style.RESET_ALL}")
     if user_answer == correct_answer:
         print_colored("Correct!", Fore.GREEN)
+        os.system("sleep 1")
         return True
     else:
         print_colored(f"Wrong! Correct answer is {correct_answer}", Fore.RED)
+        os.system("sleep 1")
         return False
 
 
 def write_wrong_questions(wrong_questions):
-    with open("wrong_questions.txt", "a") as f:
+    with open(WRONG_QUESTIONS_FILE, "a") as f:
         for question in wrong_questions:
             f.write(str(question) + "\n")
 
 
 def practice_mode():
     questions = []
-    with open("wrong_questions.txt", "r") as file:
+    with open(WRONG_QUESTIONS_FILE, "r") as file:
         for line in file:
             try:
                 questions.append(int(line.strip()))
@@ -57,8 +65,8 @@ def practice_mode():
 
     questions = list(dict.fromkeys(sorted(questions)))
 
-    with open("input.txt", "r") as f:
-        for line in f:
+    with open(INPUT_FILE, "r") as file:
+        for line in file:
             line_type = parse_line(line)
             if not line_type:
                 continue
@@ -66,12 +74,19 @@ def practice_mode():
             if line_type == "question":
                 question_number = int(line.split(".")[0])
                 if question_number in questions:
-                    result = ask_question(line, [next(f) for _ in range(5)], get_answer(question_number))
+                    result = ask_question(
+                        line,
+                        [next(file) for _ in range(5)],
+                        get_answer(question_number),
+                    )
                     if result:
                         questions.remove(question_number)
                         if not questions:
-                            print_colored("You answered all questions correctly!", Fore.GREEN)
+                            print_colored(
+                                "You answered all questions correctly!", Fore.GREEN
+                            )
                             return
+
 
 def normal_mode(start_question, end_qustion):
     print_flag = False
@@ -79,8 +94,8 @@ def normal_mode(start_question, end_qustion):
     question_number = 0
     wrong_questions = []
 
-    with open("input.txt", "r") as f:
-        for line in f:
+    with open(INPUT_FILE, "r", encoding="latin-1") as file:
+        for line in file:
             line_type = parse_line(line)
             if not line_type:
                 continue
@@ -92,43 +107,43 @@ def normal_mode(start_question, end_qustion):
                 print_flag = True
             if question_number == end_qustion + 1:
                 questions_total = end_qustion - start_question + 1
-                print_colored(f"\nYou answered {question_count}/{questions_total}", Fore.CYAN)
+                print_colored(
+                    f"\nYou answered {question_count}/{questions_total}", Fore.CYAN
+                )
                 write_wrong_questions(wrong_questions)
                 print_flag = False
                 return
 
             if print_flag:
-                result = ask_question(line, [next(f) for _ in range(5)],
-                                      get_answer(question_number))
+                result = ask_question(
+                    line, [next(file) for _ in range(5)], get_answer(question_number)
+                )
                 if result:
                     question_count += 1
                 else:
                     wrong_questions.append(int(line.split(".")[0]))
 
 
+def main(args):
+    if args.b >= 0:
+        args.s = args.b * 10
+        args.e = args.b * 10 + 9
 
-def main(start_question, end_qustion, block, practice):
-    if block >= 0:
-        start_question = block * 10
-        end_qustion = block * 10 + 9
-
-    if practice:
+    if args.practice:
         practice_mode()
     else:
-        normal_mode(start_question, end_qustion)
-
+        normal_mode(args.s, args.e)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", type=int, default=1, help="start question number")
-    parser.add_argument("-e", type=int, default=285, help="end question number")
-    parser.add_argument("-b", type=int, default=-1, help="block number")
+    parser.add_argument("-s", type=int, default=1, help="Start question number")
+    parser.add_argument("-e", type=int, default=285, help="End question number")
+    parser.add_argument("-b", type=int, default=-1, help="Block number")
     parser.add_argument("--practice", action="store_true", help="Practice mode")
     args = parser.parse_args()
 
     try:
-        main(args.s, args.e, args.b, args.practice)
+        main(args)
     except KeyboardInterrupt:
-        print_colored("\nExiting...", Fore.RED)
-
+        print_colored("\nExiting without saving...", Fore.RED)
