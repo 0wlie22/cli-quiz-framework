4d90984def43c4bb37fb8d2fdaf0fd2f77d8e871
0wlie22 <dari.sedova@gmail.com>
2023-08-10 14:52:29 +0300

    Add python linter

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+"""
+    This script is used to practice questions from the input file.
+    It can be used in two modes:
+        1. Normal mode: Practicing questions from start to end
+        2. Practice mode: Practicing questions from wrong_questions.txt file
+    Usage:
+        python3 main.py -s 1 -e 10
+        python3 main.py -b 1
+        python3 main.py --practice
+    Note:
+        1. If you want to practice questions from a specific block, use -b option
+        2. If you want to practice questions from a specific range, use -s and -e options
+        3. If you want to practice questions from wrong_questions.txt file, use --practice option
+"""
+
 import argparse
 import os
 import re
@@ -9,10 +24,28 @@ WRONG_QUESTIONS_FILE = "wrong_questions.txt"
 
 
 def print_colored(text, color=Fore.WHITE) -> None:
+    """
+    Print text in color
+
+    Args:
+        text (str): Text to print
+        color (str, optional): Color to print text in. Defaults to Fore.WHITE.
+    """
     print(f"{color}{text}{Style.RESET_ALL}")
 
 
 def parse_line(line: str) -> str:
+    """
+    Parse line to determine if it is a question or an answer
+
+    Args:
+        line (str): Line to parse
+
+    Returns:
+        str: "question" if line is a question, "answer" if line is an answer, "" otherwise
+
+    """
+
     # if line contains arabic number
     if re.search(r"^[0-9]*\.", line):
         return "question"
@@ -24,38 +57,69 @@ def parse_line(line: str) -> str:
 
 
 def get_answer(question_number: int) -> str:
-    with open("answers.txt", "r") as f:
-        answers = f.readline()
+    """
+
+    Args:
+        question_number (int): Question number
+
+    Returns:
+        str: Correct answer for the question
+
+    """
+    with open("answers.txt", "r", encoding="utf-8") as file:
+        answers = file.readline()
         return answers[question_number - 1]
 
 
 def ask_question(question, answers, correct_answer) -> bool:
+    """
+    Ask question and check if answer is correct
+
+    Args:
+        question (str): Question to ask
+        answers (list): List of answers
+        correct_answer (str): Correct answer
+
+    Returns:
+        bool: True if answer is correct, False otherwise
+    """
     os.system("clear")
     print_colored("\n" + question.strip(), Fore.CYAN)
     for _, answer in enumerate(answers):
         print_colored(answer.strip())
     user_answer = input(f"{Fore.YELLOW}Enter your answer: {Style.RESET_ALL}")
     while user_answer not in ["a", "b", "c", "d"]:
-        user_answer = input(f"{Fore.RED}Wrong answer!\n{Fore.YELLOW}Enter your answer: {Style.RESET_ALL}")
+        user_answer = input(
+            f"{Fore.RED}Wrong answer!\n{Fore.YELLOW}Enter your answer: {Style.RESET_ALL}"
+        )
     if user_answer == correct_answer:
         print_colored("Correct!", Fore.GREEN)
         os.system("sleep 1")
         return True
-    else:
-        print_colored(f"Wrong! Correct answer is {correct_answer}", Fore.RED)
-        os.system("sleep 1")
-        return False
+    print_colored(f"Wrong! Correct answer is {correct_answer}", Fore.RED)
+    os.system("sleep 1")
+    return False
 
 
-def write_wrong_questions(wrong_questions):
-    with open(WRONG_QUESTIONS_FILE, "a") as f:
+def write_wrong_questions(wrong_questions) -> None:
+    """
+    Write wrong questions to wrong_questions.txt file
+
+    Args:
+        wrong_questions (list): List of wrong questions
+
+    """
+    with open(WRONG_QUESTIONS_FILE, "a", encoding="utf-8") as file:
         for question in wrong_questions:
-            f.write(str(question) + "\n")
+            file.write(str(question) + "\n")
 
 
-def practice_mode():
+def practice_mode() -> None:
+    """
+    Practice questions from wrong_questions.txt file
+    """
     questions = []
-    with open(WRONG_QUESTIONS_FILE, "r") as file:
+    with open(WRONG_QUESTIONS_FILE, "r", encoding="utf-8") as file:
         for line in file:
             try:
                 questions.append(int(line.strip()))
@@ -65,7 +129,7 @@ def practice_mode():
 
     questions = list(dict.fromkeys(sorted(questions)))
 
-    with open(INPUT_FILE, "r") as file:
+    with open(INPUT_FILE, "r", encoding="utf-8") as file:
         for line in file:
             line_type = parse_line(line)
             if not line_type:
@@ -88,7 +152,14 @@ def practice_mode():
                             return
 
 
-def normal_mode(start_question, end_qustion):
+def normal_mode(start_question, end_qustion) -> None:
+    """
+    Practice questions from start_question to end_question
+
+    Args:
+        start_question (int): Start question number
+        end_qustion (int): End question number
+    """
     print_flag = False
     question_count = 0
     question_number = 0
@@ -125,6 +196,9 @@ def normal_mode(start_question, end_qustion):
 
 
 def main(args):
+    """
+    Main function
+    """
     if args.b >= 0:
         args.s = args.b * 10
         args.e = args.b * 10 + 9
@@ -141,9 +215,9 @@ if __name__ == "__main__":
     parser.add_argument("-e", type=int, default=285, help="End question number")
     parser.add_argument("-b", type=int, default=-1, help="Block number")
     parser.add_argument("--practice", action="store_true", help="Practice mode")
-    args = parser.parse_args()
+    arguments = parser.parse_args()
 
     try:
-        main(args)
+        main(arguments)
     except KeyboardInterrupt:
         print_colored("\nExiting without saving...", Fore.RED)
